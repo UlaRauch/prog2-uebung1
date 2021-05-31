e79d1f15f7fa3ff91b368b4904e5dcece5fd0b71
UlaRauch <71543978+UlaRauch@users.noreply.github.com>
2021-05-31 12:52:45 +0200

    fertig

--- src/main/java/PasswordChecker.java
+++ src/main/java/PasswordChecker.java
@@ -3,58 +3,51 @@ import java.util.regex.Pattern;
 
 public class PasswordChecker {
 
-    /*
-    true, if all requirements are fulfilled
-     */
+    //true, if all requirements are fulfilled
     public boolean isValid(String pw) {
-        return checkLength(pw) && checkSmallCapital(pw) && checkDigits(pw) && checkSpecialChars(pw) && noOtherSpecialChars(pw);
+        return checkLength(pw)
+                && checkLowerCase(pw)
+                && checkUpperCase(pw)
+                && checkDigits(pw)
+                && checkSpecialChars(pw)
+                && noOtherSpecialChars(pw)
+                && checkNumberContinuation(pw)
+                && check3sameNumbersMax(pw);
     }
 
-    /*
-returns true, if password has not less than 8 and not more than 25 characters
- */
+    //true, if password has not less than 8 and not more than 25 characters
     public boolean checkLength(String password) {
         return password.matches("^.{8,25}$");
     }
 
-    /*
-    returns true, if both, upper case and lower case characters are present
-     */
-    public boolean checkSmallCapital(String password) {
-        Pattern regexLC = Pattern.compile(".*[A-Z].*");
-        Pattern regexUC = Pattern.compile(".*[a-z].*");
-        boolean lc = regexLC.matcher(password).matches();
-        boolean uc = regexUC.matcher(password).matches();
-        return  lc && uc;
-        // why does this not work? return password.matches("(?=.*[a-z])(?=.*[A-Z])");
+    //true, if lower case is present
+    public boolean checkLowerCase(String password) {
+        return password.matches(".*[a-z].*");
     }
 
-    /*
-    true, if at least two digits are present (Zahlen im Plural)
-     */
+    //true, if upper case is present
+    public boolean checkUpperCase(String password) {
+        return password.matches(".*[A-Z].*");
+    }
+
+    //true, if at least two digits are present (Zahlen im Plural)
     public boolean checkDigits(String password) {
-        return password.matches("(.*\\d.*){2,}");
+        return password.matches(".*\\d.*");
     }
 
-    /*
-    true, if at least one of the required special chars is present
-     */
+    //true, if at least one of the required special chars is present
     public boolean checkSpecialChars(String password) {
-        Pattern regexSpecialC = Pattern.compile(".*[()#$?!%/@].*");
-        return regexSpecialC.matcher(password).matches();
+        return password.matches(".*[()#$?!%/@].*");
         //why does this not work? return password.matches("[()#$?!%/@]+");
     }
 
-    /*
-    true, if no other special chars than those allowed are present
-    => requirement fulfilled
-     */
+    //true, if no other special chars than those allowed are present
     public boolean noOtherSpecialChars(String password) {
         boolean noOtherSpecialCharsPresent = false;
         int otherSpecials = 0;
         String[] splitPW = password.split("");
 
-        for (String s: splitPW) {
+        for (String s : splitPW) {
             //add count, if special char is present
             if (s.matches("[\\W]")) {
                 otherSpecials++;
@@ -68,18 +61,50 @@ returns true, if password has not less than 8 and not more than 25 characters
         return noOtherSpecialCharsPresent;
     }
 
-    /*
-    false, if more than two consecutive numbers differ in +1
-     */
+    //false, if more than two consecutive numbers differ in +1
     public boolean checkNumberContinuation(String password) {
         for (int i = 0; i < password.length(); i++) {
             if (String.valueOf(password.charAt(i)).matches("[0-9]")) {
-                if ((password.charAt(i) + 1 == password.charAt(i+1)) && (password.charAt(i)+2 == password.charAt(i+2))) {
+                if ((password.charAt(i) + 1 == password.charAt(i + 1))
+                        && (password.charAt(i) + 2 == password.charAt(i + 2))) {
                     return false;
                 }
             }
         }
         return true;
     }
+
+    //false, if more than three digits in a row are the same
+    public boolean check3sameNumbersMax(String password) {
+        for (int i = 0; i < password.length() - 1; i++) {
+            if (String.valueOf(password.charAt(i)).matches("\\d")) {
+                for (int j = i; j < i+4; j++) {
+                    if (password.charAt(j) != password.charAt(j + 1)) {
+                        break;
+                    } else if (j-i == 2) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /*
+    Garage (not currently in use)
+    /*
+     /*
+    returns true, if both, upper case and lower case characters are present
+     */
+    /*
+    public boolean checkSmallCapital(String password) {
+        Pattern regexLC = Pattern.compile(".*[A-Z].*");
+        Pattern regexUC = Pattern.compile(".*[a-z].*");
+        boolean lc = regexLC.matcher(password).matches();
+        boolean uc = regexUC.matcher(password).matches();
+        return  lc && uc;
+        // why does this not work? return password.matches("(?=.*[a-z])(?=.*[A-Z])");
+    }
+    */
 
 }
--- src/test/java/PasswordCheckerTest.java
+++ src/test/java/PasswordCheckerTest.java
@@ -8,16 +8,22 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PasswordCheckerTest {
 
     @Test
-    @DisplayName("Does password fulfill ALL requirements")
+    @DisplayName("Are all requirements checked")
     public void testValidPW() {
         PasswordChecker password = new PasswordChecker();
         assertTrue(password.isValid("P4$$w0rd"));
+    }
+
+    @Test
+    @DisplayName("Are all requirements checked")
+    public void testNotValidPW() {
+        PasswordChecker password = new PasswordChecker();
         assertFalse(password.isValid("password="));
     }
 
     @Test
     @DisplayName("Is password length valid")
-    public void testPasswordLength() {
+    public void testPasswordLengthOK() {
         PasswordChecker password = new PasswordChecker();
         assertTrue(password.checkLength("Pw1234567?!"));
         assertFalse(password.checkLength("1234567"));
@@ -25,17 +31,60 @@ public class PasswordCheckerTest {
     }
 
     @Test
-    @DisplayName("Does password contain both small and capital letters")
-    public void testPasswordCase() {
+    @DisplayName("Is password too short")
+    public void testPasswordTooShort() {
         PasswordChecker password = new PasswordChecker();
-        assertTrue(password.checkSmallCapital("Pw1234567?!"));
+        assertFalse(password.checkLength("1234567"));
     }
+
+    @Test
+    @DisplayName("Is password too long")
+    public void testPasswordTooLong() {
+        PasswordChecker password = new PasswordChecker();
+        assertFalse(password.checkLength("12345678901234567890123456"));
+    }
+
+    @Test
+    @DisplayName("Does password contain lower case characters")
+    public void testPasswordLowerCase() {
+        PasswordChecker password = new PasswordChecker();
+        assertTrue(password.checkLowerCase("password"));
+    }
+
+    @Test
+    @DisplayName("Does password contain no lower case characters")
+    public void testPasswordNoLowerCase() {
+        PasswordChecker password = new PasswordChecker();
+        assertFalse(password.checkLowerCase("P455W0RD"));
+    }
+
+    @Test
+    @DisplayName("Does password contain upper case characters")
+    public void testPasswordUpperCase() {
+        PasswordChecker password = new PasswordChecker();
+        assertTrue(password.checkUpperCase("Password"));
+    }
+
+    @Test
+    @DisplayName("Does password contain no upper case characters")
+    public void testPasswordNoUpperCase() {
+        PasswordChecker password = new PasswordChecker();
+        assertFalse(password.checkUpperCase("p455word"));
+    }
+
 
     @Test
     @DisplayName("Does password contain numbers")
     public void testPasswordNumbers() {
         PasswordChecker password = new PasswordChecker();
         assertTrue(password.checkDigits("Pw1234567?!"));
+        assertFalse(password.checkDigits("Password"));
+    }
+
+    @Test
+    @DisplayName("Does password contain no numbers")
+    public void testPasswordNoNumbers() {
+        PasswordChecker password = new PasswordChecker();
         assertFalse(password.checkDigits("Password"));
     }
 
@@ -48,6 +97,13 @@ public class PasswordCheckerTest {
     }
 
     @Test
+    @DisplayName("Does password contain no special characters")
+    public void testPasswordNoSpecialChars() {
+        PasswordChecker password = new PasswordChecker();
+        assertFalse(password.checkSpecialChars("Password"));
+    }
+
+    @Test
     @DisplayName("Does not contain any other special characters")
     public void testPasswordOtherSpecialChars() {
         PasswordChecker password = new PasswordChecker();
@@ -56,19 +112,37 @@ public class PasswordCheckerTest {
     }
 
     @Test
-    @DisplayName("max two continuing numbers")
+    @DisplayName("Does not contain no other special characters")
+    public void testPasswordNoOtherSpecialChars() {
+        PasswordChecker password = new PasswordChecker();
+        assertTrue(password.noOtherSpecialChars("P4$$w0rd"));
+    }
+
+    @Test
+    @DisplayName("more than two continuing numbers")
     public void testNumberContinuation() {
         PasswordChecker password = new PasswordChecker();
         assertFalse(password.checkNumberContinuation("123Password?"));
+    }
+
+    @Test
+    @DisplayName("max two continuing numbers")
+    public void testNoNumberContinuation() {
+        PasswordChecker password = new PasswordChecker();
         assertTrue(password.checkNumberContinuation("245Password?"));
     }
-/*
+
     @Test
     @DisplayName("max 3 occurences of same digit in a row")
     public void test3samedigitsmax() {
         PasswordChecker password = new PasswordChecker();
-        assertFalse(password.check3sameDigits("P4444ssword"));
-        assertTrue(password.ch);
+        assertTrue(password.check3sameNumbersMax("P444555w0rd?4"));
     }
-*/
+
+    @Test
+    @DisplayName("more than 3 occurences of same digit in a row")
+    public void testOver3samedigitsmax() {
+        PasswordChecker password = new PasswordChecker();
+        assertFalse(password.check3sameNumbersMax("P4444ssword"));
+    }
 }
